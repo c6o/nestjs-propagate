95429ecb550533a1ae42c01f9b764c55b5b8d89d
Grant Burnard <grant@burnard.com>
2022-04-20 11:56:02 -0700

    feature: Added proof of concept for x-c6o-intercept header propagation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    // make outbound call via any http library
+    // expect specific headers to propagate
+    const result = await axios.get('https://manytools.org/http-html-text/http-request-headers/');
+    return result.data;
   }
 }
