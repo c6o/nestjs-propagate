597c2d6c45cf36e1635ff7383a1c81095aaf6725
Grant Burnard <grant@burnard.com>
2022-04-20 11:46:19 -0700

    feature: Header propagation added for x-c6o-intercept

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as hpropagate from 'hpropagate';
 
 async function bootstrap() {
+  hpropagate({
+    setAndPropagateCorrelationId: false,
+    headersToPropagate: ['x-c6o-intercept'],
+  });
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
